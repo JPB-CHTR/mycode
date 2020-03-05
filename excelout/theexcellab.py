e5af8c219b789e879569dddd119adc9d83e337ac
Joseph Brown <joseph.brown1@charter.com>
2020-03-05 15:17:55 -0500

    excel 2

--- excelout/theexcellab.py
+++ excelout/theexcellab.py
@@ -2,22 +2,20 @@
 
 import pyexcel
 
-def make_headers():
-    input_col1 = input("Enter Label for 1st column")
-    input_col2 = input("Enter Label for 2nd column")
-
-def get_ip_data():
+def get_ip_data(head1,head2):
     input_ip = input("\nWhat is the IP address? ")
     input_driver = input("What isthe driver associated with this device? ")
-    d = {"IP": input_ip, "driver": input_driver}
+    d = {head1 : input_ip, head2 : input_driver}
     return d
 
 mylistdict = []
 
 print("Hello! This program will make you a *.xls file")
+col_head1 = input("What would you like to name column 1? ")
+col_head2 = input("What would you like to name column 2? ")
 
 while(True):
-    mylistdict.append(get_ip_data()) # add an item to the list returned by get_ip_data() {"IP": value, "driver": value}
+    mylistdict.append(get_ip_data(col_head1,col_head2)) # add an item to the list returned by get_ip_data() {"IP": value, "driver": value}
     keep_going = input("\nWould you like to add another value? Enter to continue, or enter 'q' to quit: ")
     if (keep_going.lower() == 'q'):
         break
